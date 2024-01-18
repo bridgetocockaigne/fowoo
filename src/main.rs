93e7f431cbbc1839d1a0d01c0a29c8d56658c2ad
Ricardo Batista <iamricardobatista@gmail.com>
2024-01-18 20:47:42 +0000

    add statics folder and favicon

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use axum::{routing::get, Router};
 use controller::home;
 use infra::state::State;
+use tower_http::services::ServeDir;
 
 mod controller;
 mod infra;
@@ -11,10 +12,19 @@ mod infra;
 async fn main() {
     let template_dir_path =
         env::var("FOWOO_TEMPLATE_DIR_PATH").unwrap_or("/workspace/templates/**/*.html".to_string());
+
     let state = State::new(&template_dir_path);
 
+    let statics_dir_path =
+        env::var("FOWOO_STATICS_DIR_PATH").unwrap_or("/workspace/statics".to_string());
+
+    let serve_dir = ServeDir::new(statics_dir_path);
+
     // build our application with a single route
-    let app = Router::new().route("/", get(home::index)).with_state(state);
+    let app = Router::new()
+        .route("/", get(home::index))
+        .with_state(state)
+        .nest_service("/statics", serve_dir);
 
     // run our app with hyper, listening globally on port 8080
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
