a988ac02c494848ca3d054eaa89566d01963bc5f
Ricardo Batista <iamricardobatista@gmail.com>
2024-01-23 20:51:21 +0000

    add login page and login link

---
+++ src/controller/login.rs
@@ -0,0 +1,12 @@
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse},
+};
+use tera::{Context, Tera};
+
+pub(crate) async fn index(State(tera): State<Tera>) -> impl IntoResponse {
+    let result = tera
+        .render("login/index.html", &Context::default())
+        .unwrap();
+    Html(result)
+}
