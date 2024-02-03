0d2915fe91546fd0a295767e398c8e06b663fe36
Ricardo Batista <iamricardobatista@gmail.com>
2024-02-03 00:16:39 +0000

    add migrations container

--- tests/fowoo.rs
+++ tests/fowoo.rs
@@ -1,18 +1,2 @@
-use std::env;
-
-use sqlx::{migrate::Migrator, postgres::PgPoolOptions};
-
-static MIGRATOR: Migrator = sqlx::migrate!();
-
 #[tokio::main]
-async fn main() {
-    let database_url = env::var("DATABASE_URL").unwrap();
-
-    let db = PgPoolOptions::new()
-        .max_connections(20)
-        .connect(&database_url)
-        .await
-        .unwrap();
-
-    MIGRATOR.run(&db).await.unwrap();
-}
+async fn main() {}
