fc66b1f88405cc5df5ac288ec158e265da8e5ad3
Ricardo Batista <iamricardobatista@gmail.com>
2024-02-07 00:22:27 +0000

    trying one first dummy test

--- src/controller/login.rs
+++ src/controller/login.rs
@@ -22,3 +22,22 @@ pub(crate) async fn google(State(google_client): State<impl Client>) -> impl Int
 
     (StatusCode::TEMPORARY_REDIRECT, headers)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use axum::{extract::State, http::StatusCode, response::IntoResponse};
+    use tera::Tera;
+
+    #[tokio::test]
+    async fn index() {
+        let template_dir_path = env::var("FOWOO_TEMPLATE_DIR_PATH")
+            .unwrap_or("/workspace/templates/**/*.html".to_string());
+
+        let tera = Tera::new(&template_dir_path).unwrap();
+        let result = super::index(State(tera)).await.into_response();
+
+        assert_eq!(StatusCode::OK, result.status());
+    }
+}
