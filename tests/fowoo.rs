8e40043146239c39e037680ed4e2102dbae35b64
Ricardo Batista <iamricardobatista@gmail.com>
2024-02-12 20:05:38 +0000

    test oauth flow

--- tests/fowoo.rs
+++ tests/fowoo.rs
@@ -1,25 +1,41 @@
 use std::{env, fs};
 
-use axum::{extract::State, http::StatusCode};
+use axum::{
+    extract::State,
+    http::{HeaderMap, StatusCode},
+};
 use cucumber::{then, when, writer, World, WriterExt as _};
 use fowoo::{controller, infra::http::Response};
 use tera::Tera;
 
-#[derive(Default, World, Debug)]
+#[derive(World, Debug)]
 struct FowooWorld {
+    tera: Tera,
     response: Response,
+}
+
+impl Default for FowooWorld {
+    fn default() -> Self {
+        let template_dir_path = env::var("FOWOO_TEMPLATE_DIR_PATH")
+            .unwrap_or("/workspace/templates/**/*.html".to_string());
+        let tera = Tera::new(&template_dir_path).unwrap();
+        let response = (StatusCode::OK, HeaderMap::new(), "".to_string());
+
+        Self { tera, response }
+    }
 }
 
 #[when(expr = "the user visits the homepage")]
 async fn the_user_vists_the_homepage(world: &mut FowooWorld) {
-    let template_dir_path =
-        env::var("FOWOO_TEMPLATE_DIR_PATH").unwrap_or("/workspace/templates/**/*.html".to_string());
-    let tera = Tera::new(&template_dir_path).unwrap();
-
-    world.response = controller::home::index(State(tera)).await;
+    world.response = controller::home::index(State(world.tera.clone())).await;
 }
 
-#[then(expr = "the return status is {int} Ok")]
+#[when(expr = "the user vists the login page")]
+async fn the_user_vists_the_login_page(world: &mut FowooWorld) {
+    world.response = controller::login::index(State(world.tera.clone())).await;
+}
+
+#[then(expr = "the return status is {int}")]
 async fn the_return_status_is(world: &mut FowooWorld, number: u16) {
     assert_eq!(StatusCode::from_u16(number).unwrap(), world.response.0)
 }
@@ -37,6 +53,6 @@ async fn main() {
                 .normalized(),
         )
         .fail_on_skipped()
-        .run("tests/features/homepage.feature")
+        .run("tests/features/login.feature")
         .await;
 }
