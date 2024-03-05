87f0add83fa529dae0a3c20357624cfcf88d4345
Ricardo Batista <iamricardobatista@gmail.com>
2024-03-05 21:41:44 +0000

    Change tera declaration to lazy static

--- src/infra.rs
+++ src/infra.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+use lazy_static::lazy_static;
+use tera::Tera;
+
 pub mod auth;
 pub mod http;
 pub mod state;
+
+lazy_static! {
+    pub static ref TEMPLATE: Tera = {
+        let template_dir_path = env::var("FOWOO_TEMPLATE_DIR_PATH")
+            .unwrap_or("/workspace/templates/**/*.html".to_string());
+        Tera::new(&template_dir_path).unwrap()
+    };
+}
--- src/infra/state.rs
+++ src/infra/state.rs
@@ -1,34 +1,22 @@
 use axum::extract::FromRef;
-use tera::Tera;
 
 use super::auth::{google, Client};
 
 #[derive(Clone)]
 pub struct State {
-    pub(crate) tera: Tera,
     pub(crate) google_client: google::Client,
 }
 
 impl State {
-    pub fn new(template_dir_path: &str) -> Self {
-        let tera = Tera::new(template_dir_path).unwrap();
+    pub fn new() -> Self {
         let google_client = google::Client::default();
 
-        Self {
-            tera,
-            google_client,
-        }
+        Self { google_client }
     }
 }
 
 impl FromRef<State> for google::Client {
     fn from_ref(input: &State) -> Self {
         input.google_client.clone()
-    }
-}
-
-impl FromRef<State> for Tera {
-    fn from_ref(input: &State) -> Self {
-        input.tera.clone()
     }
 }
